44c210e56fcc7f29337a8e24653a460305a5cee8
rwt-9829 <hansenhe2008@gmail.com>
2025-06-24 15:33:05 -0700

    update hsm with eventchecker

--- GroupActivity1/ES_Framework/ES_Framework_HSM.X/ES_Configure.h
+++ GroupActivity1/ES_Framework/ES_Framework_HSM.X/ES_Configure.h
@@ -70,11 +70,11 @@ static const char *EventNames[] = {
 
 /****************************************************************************/
 // This are the name of the Event checking function header file.
-#define EVENT_CHECK_HEADER "ES_Configure.h"
+#define EVENT_CHECK_HEADER "TemplateEventChecker.h"
 
 /****************************************************************************/
 // This is the list of event checking functions
-#define EVENT_CHECK_LIST 
+#define EVENT_CHECK_LIST TemplateCheckBattery
 
 /****************************************************************************/
 // These are the definitions for the post functions to be executed when the
---
+++ GroupActivity1/ES_Framework/ES_Framework_HSM.X/build/default/production/_ext/1230780070/TemplateEventChecker.o.d
@@ -0,0 +1,21 @@
+build/default/production/_ext/1230780070/TemplateEventChecker.o: \
+ C:/Users/hanse/MPLABXProjects/ES_Framework_HSM.X/TemplateEventChecker.c \
+ C:/Users/hanse/MPLABXProjects/ES_Framework_HSM.X/ES_Configure.h \
+ C:/Users/hanse/MPLABXProjects/ES_Framework_HSM.X/TemplateEventChecker.h \
+ C:/ECE118/include/BOARD.h C:/ECE118/include/GenericTypeDefs.h \
+ C:/ECE118/include/ES_Events.h C:/ECE118/include/serial.h \
+ C:/ECE118/include/AD.h
+
+C:/Users/hanse/MPLABXProjects/ES_Framework_HSM.X/ES_Configure.h:
+
+C:/Users/hanse/MPLABXProjects/ES_Framework_HSM.X/TemplateEventChecker.h:
+
+C:/ECE118/include/BOARD.h:
+
+C:/ECE118/include/GenericTypeDefs.h:
+
+C:/ECE118/include/ES_Events.h:
+
+C:/ECE118/include/serial.h:
+
+C:/ECE118/include/AD.h:
