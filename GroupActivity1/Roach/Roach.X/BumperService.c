3762983dcad3682b4261510a815be5f51c201700
rwt-9829 <hansenhe2008@gmail.com>
2025-06-25 13:43:39 -0700

    add bumper service functionality

--- GroupActivity1/Roach/Roach.X/BumperService.c
+++ GroupActivity1/Roach/Roach.X/BumperService.c
@@ -1,75 +1,37 @@
 /*
- * File: TemplateService.h
- * Author: J. Edward Carryer
- * Modified: Gabriel H Elkaim
- *
- * Template file to set up a simple service to work with the Events and Services
- * Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that this file
- * will need to be modified to fit your exact needs, and most of the names will have
- * to be changed to match your code.
- *
- * This is provided as an example and a good place to start.
- *
- * Created on 23/Oct/2011
- * Updated on 13/Nov/2013
+ * File:   BumperService.c
+ * Author: Your Name
+ * 
+ * Description:
+ * Debounced bumper polling service. Periodically reads bumpers and
+ * posts directional bump events when transitions are detected.
  */
 
-/*******************************************************************************
- * MODULE #INCLUDE                                                             *
- ******************************************************************************/
-
 #include "BOARD.h"
-#include "AD.h"
 #include "ES_Configure.h"
 #include "ES_Framework.h"
-#include "TemplateService.h"
+#include "BumperService.h"
+#include "roach.h"
+#include "RoachHSM.h" // Needed to post to top-level HSM
 #include <stdio.h>
 
-/*******************************************************************************
- * MODULE #DEFINES                                                             *
- ******************************************************************************/
-
-#define BATTERY_DISCONNECT_THRESHOLD 175
-
-/*******************************************************************************
- * PRIVATE FUNCTION PROTOTYPES                                                 *
- ******************************************************************************/
-/* Prototypes for private functions for this machine. They should be functions
-   relevant to the behavior of this state machine */
-
-/*******************************************************************************
- * PRIVATE MODULE VARIABLES                                                    *
- ******************************************************************************/
-/* You will need MyPriority and maybe a state variable; you may need others
- * as well. */
+#define BUMPER_TIMER 0        // Make sure this matches ES_Configure.h
+#define POLL_INTERVAL 5       // ms
+#define BUMPER_MASK 0x0F      // Mask for 4-bit bumper input
 
 static uint8_t MyPriority;
+static uint8_t LastBumperState = 0x00;
 
 /*******************************************************************************
- * PUBLIC FUNCTIONS                                                            *
+ * PUBLIC FUNCTIONS
  ******************************************************************************/
 
-/**
- * @Function InitTemplateService(uint8_t Priority)
- * @param Priority - internal variable to track which event queue to use
- * @return TRUE or FALSE
- * @brief This will get called by the framework at the beginning of the code
- *        execution. It will post an ES_INIT event to the appropriate event
- *        queue, which will be handled inside RunTemplateService function. Remember
- *        to rename this to something appropriate.
- *        Returns TRUE if successful, FALSE otherwise
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-uint8_t InitTemplateService(uint8_t Priority)
-{
+uint8_t InitBumperService(uint8_t Priority) {
     ES_Event ThisEvent;
-
     MyPriority = Priority;
 
-    // in here you write your initialization code
-    // this includes all hardware and software initialization
-    // that needs to occur.
+    ES_Timer_InitTimer(BUMPER_TIMER, POLL_INTERVAL);
 
-    // post the initial transition event
     ThisEvent.EventType = ES_INIT;
     if (ES_PostToService(MyPriority, ThisEvent) == TRUE) {
         return TRUE;
@@ -78,78 +40,49 @@ uint8_t InitTemplateService(uint8_t Priority)
     }
 }
 
-/**
- * @Function PostTemplateService(ES_Event ThisEvent)
- * @param ThisEvent - the event (type and param) to be posted to queue
- * @return TRUE or FALSE
- * @brief This function is a wrapper to the queue posting function, and its name
- *        will be used inside ES_Configure to point to which queue events should
- *        be posted to. Remember to rename to something appropriate.
- *        Returns TRUE if successful, FALSE otherwise
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-uint8_t PostTemplateService(ES_Event ThisEvent)
-{
+uint8_t PostBumperService(ES_Event ThisEvent) {
     return ES_PostToService(MyPriority, ThisEvent);
 }
 
-/**
- * @Function RunTemplateService(ES_Event ThisEvent)
- * @param ThisEvent - the event (type and param) to be responded.
- * @return Event - return event (type and param), in general should be ES_NO_EVENT
- * @brief This function is where you implement the whole of the service,
- *        as this is called any time a new event is passed to the event queue. 
- * @note Remember to rename to something appropriate.
- *       Returns ES_NO_EVENT if the event have been "consumed." 
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-ES_Event RunTemplateService(ES_Event ThisEvent)
-{
+ES_Event RunBumperService(ES_Event ThisEvent) {
+    static ES_EventTyp_t lastEvent = NB;
     ES_Event ReturnEvent;
-    ReturnEvent.EventType = ES_NO_EVENT; // assume no errors
-
-    /********************************************
-     in here you write your service code
-     *******************************************/
-    static ES_EventTyp_t lastEvent = BATTERY_DISCONNECTED;
-    ES_EventTyp_t curEvent;
-    uint16_t batVoltage = AD_ReadADPin(BAT_VOLTAGE); // read the battery voltage
+    ReturnEvent.EventType = ES_NO_EVENT;
 
     switch (ThisEvent.EventType) {
-    case ES_INIT:
-        // No hardware initialization or single time setups, those
-        // go in the init function above.
-        //
-        // This section is used to reset service for some reason
-        break;
+        case ES_INIT:
+            // Start polling
+            break;
 
-    case ES_TIMEOUT:
-        if (batVoltage > BATTERY_DISCONNECT_THRESHOLD) { // is battery connected?
-            curEvent = BATTERY_CONNECTED;
-        } else {
-            curEvent = BATTERY_DISCONNECTED;
+        case ES_TIMEOUT: {
+            ES_Timer_InitTimer(BUMPER_TIMER, POLL_INTERVAL);
+
+            uint8_t CurrentBumper = Roach_ReadBumpers() & BUMPER_MASK;
+            ES_EventTyp_t curEvent = NB;
+
+            switch (CurrentBumper) {
+                case 0x01: curEvent = FL; break;
+                case 0x02: curEvent = FR; break;
+                case 0x04: curEvent = BL; break;
+                case 0x08: curEvent = BR; break;
+                case 0x03: curEvent = FRONT; break; // FL + FR
+                case 0x0C: curEvent = BACK;  break; // BL + BR
+                case 0x00: curEvent = NB;    break;
+                default:   curEvent = NB;    break;
+            }
+
+            if (curEvent != lastEvent) {
+                lastEvent = curEvent;
+                ReturnEvent.EventType = curEvent;
+                ReturnEvent.EventParam = CurrentBumper;
+                PostRoachHSM(ReturnEvent);
+            }
+            break;
         }
-        if (curEvent != lastEvent) { // check for change from last time
-            ReturnEvent.EventType = curEvent;
-            ReturnEvent.EventParam = batVoltage;
-            lastEvent = curEvent; // update history
-#ifndef SIMPLESERVICE_TEST           // keep this as is for test harness
-            PostGenericService(ReturnEvent);
-#else
-            PostTemplateService(ReturnEvent);
-#endif   
-        }
-        break;
-#ifdef SIMPLESERVICE_TEST     // keep this as is for test harness      
-    default:
-        printf("\r\nEvent: %s\tParam: 0x%X",
-                EventNames[ThisEvent.EventType], ThisEvent.EventParam);
-        break;
-#endif
+
+        default:
+            break;
     }
 
     return ReturnEvent;
 }
-
-/*******************************************************************************
- * PRIVATE FUNCTIONs                                                           *
- ******************************************************************************/
-
--- GroupActivity1/Roach/Roach.X/BumperService.h
+++ GroupActivity1/Roach/Roach.X/BumperService.h
@@ -1,79 +1,43 @@
 /*
- * File: TemplateService.h
- * Author: J. Edward Carryer
- * Modified: Gabriel H Elkaim
- *
- * Template file to set up a simple service to work with the Events and Services
- * Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that this file
- * will need to be modified to fit your exact needs, and most of the names will have
- * to be changed to match your code.
- *
- * This is provided as an example and a good place to start.
- *
- * Created on 23/Oct/2011
- * Updated on 13/Nov/2013
+ * File:   BumperService.h
+ * Author: Your Name
+ * 
+ * Description:
+ * Bumper polling service for Roach robot using the ES_Framework.
+ * Checks bumper inputs every 5ms and posts directional bump events to the HSM.
  */
 
-#ifndef TemplateService_H  // <- This should be changed to your own guard on both
-#define TemplateService_H  //    of these lines
+#ifndef BumperService_H
+#define BumperService_H
 
-
-/*******************************************************************************
- * PUBLIC #INCLUDES                                                            *
- ******************************************************************************/
-
-#include "ES_Configure.h"   // defines ES_Event, INIT_EVENT, ENTRY_EVENT, and EXIT_EVENT
+#include "ES_Configure.h"
 
 /*******************************************************************************
- * PUBLIC #DEFINES                                                             *
+ * PUBLIC FUNCTION PROTOTYPES
  ******************************************************************************/
-
-
-/*******************************************************************************
- * PUBLIC TYPEDEFS                                                             *
- ******************************************************************************/
-
-
-/*******************************************************************************
- * PUBLIC FUNCTION PROTOTYPES                                                  *
- ******************************************************************************/
-
- 
-/**
- * @Function InitTemplateService(uint8_t Priority)
- * @param Priority - internal variable to track which event queue to use
- * @return TRUE or FALSE
- * @brief This will get called by the framework at the beginning of the code
- *        execution. It will post an ES_INIT event to the appropriate event
- *        queue, which will be handled inside RunTemplateService function. Remember
- *        to rename this to something appropriate.
- *        Returns TRUE if successful, FALSE otherwise
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-uint8_t InitTemplateService(uint8_t Priority);
 
 /**
- * @Function PostTemplateService(ES_Event ThisEvent)
- * @param ThisEvent - the event (type and param) to be posted to queue
- * @return TRUE or FALSE
- * @brief This function is a wrapper to the queue posting function, and its name
- *        will be used inside ES_Configure to point to which queue events should
- *        be posted to. Remember to rename to something appropriate.
- *        Returns TRUE if successful, FALSE otherwise
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-uint8_t PostTemplateService(ES_Event ThisEvent);
+ * @Function InitBumperService(uint8_t Priority)
+ * @param Priority - priority of this service's event queue
+ * @return TRUE if initialized successfully, FALSE otherwise
+ * @brief Initializes the service and starts the bumper polling timer
+ */
+uint8_t InitBumperService(uint8_t Priority);
 
 /**
- * @Function RunTemplateService(ES_Event ThisEvent)
- * @param ThisEvent - the event (type and param) to be responded.
- * @return Event - return event (type and param), in general should be ES_NO_EVENT
- * @brief This function is where you implement the whole of the service,
- *        as this is called any time a new event is passed to the event queue. 
- * @note Remember to rename to something appropriate.
- *       Returns ES_NO_EVENT if the event have been "consumed." 
- * @author J. Edward Carryer, 2011.10.23 19:25 */
-ES_Event RunTemplateService(ES_Event ThisEvent);
+ * @Function PostBumperService(ES_Event ThisEvent)
+ * @param ThisEvent - the event (type and param) to be posted to the service
+ * @return TRUE if successful, FALSE otherwise
+ * @brief Wrapper to post events to this service's queue
+ */
+uint8_t PostBumperService(ES_Event ThisEvent);
 
+/**
+ * @Function RunBumperService(ES_Event ThisEvent)
+ * @param ThisEvent - the event (type and param) to be processed
+ * @return ES_NO_EVENT normally, or the event if forwarded
+ * @brief Processes polling timer expirations and posts bump events
+ */
+ES_Event RunBumperService(ES_Event ThisEvent);
 
-
-#endif /* TemplateService_H */
-
+#endif /* BumperService_H */
