92ff8dc960a6849a3442c53cbaf548a937e0df8f
rwt-9829 <hansenhe2008@gmail.com>
2025-07-09 18:32:23 -0400

    raspberry pi + apriltag stuf

---
+++ apriltag/pi.py
@@ -0,0 +1,18 @@
+# file on the raspberry pi that just takes a pic
+# WILL NOT RUN LOCALLy
+from picamera2 import Picamera2
+from time import sleep
+
+# Create the camera object
+picam2 = Picamera2()
+
+# Configure preview (optional)
+picam2.start()
+
+# Wait for camera to adjust settings
+sleep(2)
+
+# Capture and save the image
+picam2.capture_file("image.jpg")
+
+print("Picture saved as image.jpg")
